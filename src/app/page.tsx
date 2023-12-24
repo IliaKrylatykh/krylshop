c01a3988c8da87925cf600ac5caac1e0f3feeb14
Ilia Krylatykh <iliakrylatykh@gmail.com>
2023-12-24 23:07:17 +0400

    test deploy main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ProductsList } from "@/features/products-list/public/products-list";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
-      <h1>Hello</h1>
+      <h1>Hello((</h1>
       <CreateProductForm
         revalidatePagePath="/"
         className="max-w-[300px] mb-10"
