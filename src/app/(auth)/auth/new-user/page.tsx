3ade899c88437122b8d413deb5d5f5410eedbad9
Ilia Krylatykh <iliakrylatykh@gmail.com>
2024-02-04 20:45:15 +0400

    favicon

--- src/app/(auth)/auth/new-user/page.tsx
+++ src/app/(auth)/auth/new-user/page.tsx
@@ -1,4 +1,4 @@
-// import { UpdateProfileForm } from "@/features/update-profile/update-profile-form";
+import { UpdateProfileForm } from "@/features/update-profile/update-profile-form";
 import { getAppSessionServer } from "@/entities/user/session.server";
 import { redirect } from "next/navigation";
 
@@ -16,15 +16,15 @@ export default async function NewUserPage({
   return (
     <main className="space-y-6 py-14 container  max-w-[600px]">
       <div>
-        <h3 className="text-lg font-medium">Последний шаг</h3>
+        <h3 className="text-lg font-medium">Last step</h3>
         <p className="text-sm text-muted-foreground">
-          Обновите профиль, это как другие пользователи увидят вас на сайте
+          Update your profile, this is how other users will see you on the site
         </p>
       </div>
-      {/* <UpdateProfileForm
+      <UpdateProfileForm
         userId={session.user.id}
         callbackUrl={searchParams.callbackUrl}
-      /> */}
+      />
     </main>
   );
 }
